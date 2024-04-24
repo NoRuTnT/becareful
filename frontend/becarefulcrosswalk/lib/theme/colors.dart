4cf1946f11c7dbff47c175bc7282ed7b25b9e277
olrlozl <thakd221@naver.com>
2024-04-24 17:51:23 +0900

    feat: 공통 색상 관리

---
+++ frontend/becarefulcrosswalk/lib/theme/colors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+const lightRed = Color(0xFFFFE5E0);
+const lightYellow = Color(0xFFFFF0D9);
+const lightGreen = Color(0xFFE6EEE1);
+const lightBlue = Color(0xFFD0E3F5);
+const lightPurple = Color(0xFFEEEBFF);
+
+const red = Color(0xFFFC5230);
+const yellow = Color(0xFFFFA41D);
+const green = Color(0xFF568A35);
+const navy = Color(0xFF292868);
+
+const black = Color(0xFF0B0B0B);
