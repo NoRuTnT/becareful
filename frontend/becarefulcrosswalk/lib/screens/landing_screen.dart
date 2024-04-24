598434adf904831efa7988f0773c42c49110d5d7
olrlozl <thakd221@naver.com>
2024-04-24 17:52:12 +0900

    feat: 랜딩 페이지

--- frontend/becarefulcrosswalk/lib/screens/landing_screen.dart
+++ frontend/becarefulcrosswalk/lib/screens/landing_screen.dart
@@ -1,10 +1,113 @@
-import 'package:flutter/cupertino.dart';
+import 'package:becarefulcrosswalk/theme/colors.dart';
+import 'package:becarefulcrosswalk/widgets/logo_widget.dart';
+import 'package:flutter/material.dart';
 
 class LandingScreen extends StatelessWidget {
   const LandingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Stack(
+                children: [
+                  Positioned.fill(
+                    child: Transform.translate(
+                      offset: Offset(0.0, 40.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('images/lamplight.png'),
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Image.asset('images/traffic-lights.png',
+                            width: 128, height: 128),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              "시각 장애인을 위한",
+                              style: TextStyle(
+                                color: navy,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w300,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text("안전 보행 서비스",
+                                style: TextStyle(
+                                  color: navy,
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w600,
+                                )),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              logo_widget(),
+              SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "시작하려면 ",
+                    style: TextStyle(
+                      color: black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w300,
+                      shadows: [
+                        Shadow(
+                          color: Colors.black.withOpacity(0.5),
+                          blurRadius: 2,
+                          offset: Offset(1, 1),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Text(
+                    "터치",
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w300,
+                      shadows: [
+                        Shadow(
+                          color: Colors.black.withOpacity(0.5),
+                          blurRadius: 2,
+                          offset: Offset(1, 1),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
