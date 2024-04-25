ad571a73783a8f181218d7749505a08a672ba12e
olrlozl <thakd221@naver.com>
2024-04-25 10:38:32 +0900

    feat: 신호 정보 페이지

--- frontend/becarefulcrosswalk/lib/theme/colors.dart
+++ frontend/becarefulcrosswalk/lib/theme/colors.dart
@@ -12,3 +12,9 @@ const green = Color(0xFF568A35);
 const navy = Color(0xFF292868);
 
 const black = Color(0xFF0B0B0B);
+const darkGray = Color(0xFF545454);
+const lightGray = Color(0XFFE4E4E4);
+const white = Color(0XFFF5F5F5);
+
+const red2 = Color(0XFFFF7053);
+const green2 = Color(0XFF67E467);
