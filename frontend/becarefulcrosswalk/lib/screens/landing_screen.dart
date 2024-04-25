dee24989f4d9dd5df126ecc319cb93312a67eb13
김지환 <wlghks98k@gmail.com>
2024-04-25 14:34:32 +0900

    feat : 라우터 추가

--- frontend/becarefulcrosswalk/lib/screens/landing_screen.dart
+++ frontend/becarefulcrosswalk/lib/screens/landing_screen.dart
@@ -2,6 +2,8 @@ import 'package:becarefulcrosswalk/theme/colors.dart';
 import 'package:becarefulcrosswalk/widgets/logo_widget.dart';
 import 'package:flutter/material.dart';
 
+import 'main_screen.dart';
+
 class LandingScreen extends StatelessWidget {
   const LandingScreen({super.key});
 
@@ -9,102 +11,114 @@ class LandingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Stack(
-                children: [
-                  Positioned.fill(
-                    child: Transform.translate(
-                      offset: Offset(0.0, 40.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: AssetImage('images/lamplight.png'),
-                            fit: BoxFit.contain,
+      body: GestureDetector(
+        behavior: HitTestBehavior.translucent,
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => MainScreen(),
+              fullscreenDialog: true,
+            ),
+          );
+        },
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Stack(
+                  children: [
+                    Positioned.fill(
+                      child: Transform.translate(
+                        offset: const Offset(0.0, 40.0),
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage('images/lamplight.png'),
+                              fit: BoxFit.contain,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  Positioned(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Image.asset('images/traffic-lights.png',
-                            width: 128, height: 128),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Column(
-                          children: [
-                            Text(
-                              "시각 장애인을 위한",
-                              style: TextStyle(
-                                color: navy,
-                                fontSize: 20,
-                                fontWeight: FontWeight.w300,
-                              ),
-                            ),
-                            SizedBox(
-                              height: 5,
-                            ),
-                            Text("안전 보행 서비스",
+                    Positioned(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Image.asset('images/traffic-lights.png',
+                              width: 128, height: 128),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const Column(
+                            children: [
+                              Text(
+                                "시각 장애인을 위한",
                                 style: TextStyle(
                                   color: navy,
-                                  fontSize: 30,
-                                  fontWeight: FontWeight.w600,
-                                )),
-                          ],
-                        ),
-                      ],
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w300,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text("안전 보행 서비스",
+                                  style: TextStyle(
+                                    color: navy,
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.w600,
+                                  )),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              logo_widget(),
-              SizedBox(
-                height: 80,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "시작하려면 ",
-                    style: TextStyle(
-                      color: black,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w300,
-                      shadows: [
-                        Shadow(
-                          color: Colors.black.withOpacity(0.5),
-                          blurRadius: 2,
-                          offset: Offset(1, 1),
-                        ),
-                      ],
+                  ],
+                ),
+                const LogoWidget(),
+                const SizedBox(
+                  height: 80,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "시작하려면 ",
+                      style: TextStyle(
+                        color: black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w300,
+                        shadows: [
+                          Shadow(
+                            color: Colors.black.withOpacity(0.5),
+                            blurRadius: 2,
+                            offset: const Offset(1, 1),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  Text(
-                    "터치",
-                    style: TextStyle(
-                      color: Colors.red,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w300,
-                      shadows: [
-                        Shadow(
-                          color: Colors.black.withOpacity(0.5),
-                          blurRadius: 2,
-                          offset: Offset(1, 1),
-                        ),
-                      ],
+                    Text(
+                      "터치",
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w300,
+                        shadows: [
+                          Shadow(
+                            color: Colors.black.withOpacity(0.5),
+                            blurRadius: 2,
+                            offset: const Offset(1, 1),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
