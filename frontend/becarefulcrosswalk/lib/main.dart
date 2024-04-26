5ac26821b4ca45a93027e4f17f43ee75b2653a7a
김지환 <wlghks98k@gmail.com>
2024-04-26 10:33:30 +0900

    feat : 이용방법 라우터추가

--- frontend/becarefulcrosswalk/lib/main.dart
+++ frontend/becarefulcrosswalk/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:becarefulcrosswalk/screens/landing_screen.dart';
 import 'package:becarefulcrosswalk/screens/map_screen.dart';
+import 'package:becarefulcrosswalk/screens/user_guide_screen.dart';
+import 'package:becarefulcrosswalk/screens/user_guide_screen2.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +18,8 @@ class App extends StatelessWidget {
         '/': (context) => LandingScreen(),
         '/map': (context) => const MapScreen(),
         // '/report': (context) => const ReportScreen(),
-        // '/userGuide' : (context) => const userGuideScreen(),
+        '/userGuide': (context) => const UserGuideScreen(),
+        '/userGuide2': (context) => const UserGuideScreen2(),
       },
       initialRoute: '/',
     );
