e8de0c283291b7dc428b2f5d3f1c609c0e46e6c0
olrlozl <thakd221@naver.com>
2024-05-02 14:10:35 +0900

    feat: firebase 연결 & 실시간 신호정보 안내

--- frontend/becarefulcrosswalk/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/becarefulcrosswalk/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
+import firebase_database
 import geolocator_apple
 import path_provider_foundation
 import shared_preferences_foundation
@@ -12,6 +14,8 @@ import speech_to_text_macos
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
