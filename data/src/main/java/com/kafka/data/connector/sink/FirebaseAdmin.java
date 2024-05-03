4e827b60019b3f9538fc82a4494f7efcfa6577c8
noowadc <cha12608@naver.com>
2024-05-03 10:21:26 +0900

    feat : firebase 설정 추가

---
+++ data/src/main/java/com/kafka/data/connector/sink/FirebaseAdmin.java
@@ -0,0 +1,44 @@
+package com.kafka.data.connector.sink;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+class FirebaseAdmin {
+
+  private DatabaseReference databaseReference;
+  private String Url = "${firebase.url}";
+  private String Path = "${firebase.path}";
+
+  FirebaseAdmin(String databaseRef) {
+    FirebaseOptions options = null;
+    try {
+      FileInputStream serviceAccount =
+          new FileInputStream(Path);
+
+      options = new FirebaseOptions.Builder()
+          .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+          .setDatabaseUrl(Url)
+          .build();
+
+      FirebaseApp.initializeApp(options);
+
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    FirebaseApp.initializeApp(options);
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    databaseReference = database.getReference(databaseRef);
+  }
+
+  void saveData(String path, String id, Object object) {
+    DatabaseReference childRef = databaseReference.child(path);
+    childRef.child(id).setValueAsync(object);
+  }
+}
