4cecf0e27eda5da6ea2978bc277fec44dafe3bda
olrlozl <thakd221@naver.com>
2024-05-08 09:55:05 +0900

    feat: 디바이스 방향의 방위각과 현위치에서 목적지 방향의 방위각 구하기

--- frontend/becarefulcrosswalk/lib/service/my_direction.dart
+++ frontend/becarefulcrosswalk/lib/service/my_direction.dart
@@ -1,24 +1,25 @@
 import 'dart:async';
 import 'dart:math';
 
-import 'package:becarefulcrosswalk/service/my_location.dart';
 import 'package:flutter_compass/flutter_compass.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:vibration/vibration.dart';
 
 class MyDirection {
-  // 방위각을 리스닝하기 위한 StreamSubscription 인스턴스
+  // 디바이스의 현재 방위각을 리스닝하기 위한 StreamSubscription 인스턴스
   StreamSubscription<CompassEvent>? _compassSubscription;
 
   // 디바이스의 현재 방위각
   double? _currentHeading;
 
-  // 방위각 리스닝 시작
+  // 디바이스의 현재 방위각 리스닝 시작
   void startListening() {
     _compassSubscription = FlutterCompass.events!.listen((CompassEvent event) {
       _currentHeading = event.heading;
     });
   }
 
-  // 방위각 리스닝 중지
+  // 디바이스의 현재 방위각 리스닝 중지
   void stopListening() {
     _compassSubscription?.cancel();
   }
@@ -29,13 +30,17 @@ class MyDirection {
   }
 
   // 현재 위치에서 목적지 방향의 방위각
-  Future<double> getAzimuthDestination(double destLat, double destLng) async {
-    MyLocation myLocation = MyLocation();
-    await myLocation.getCurrentLocation();
-    return calculateBearing(
-        myLocation.latitude, myLocation.longitude, destLat, destLng);
+  double? _azimuthDestination;
+
+  // 현재 위치에서 목적지 방향의 방위각 리스닝 시작
+  void startLocationUpdates(double destLat, double destLng) {
+    Geolocator.getPositionStream().listen((Position myLocation) {
+      _azimuthDestination = calculateBearing(
+          myLocation.latitude, myLocation.longitude, destLat, destLng);
+    });
   }
 
+  // 두 지점 사이의 방위각 계산
   double calculateBearing(
       double startLat, double startLng, double endLat, double endLng) {
     // 라디안으로 변환
@@ -62,5 +67,44 @@ class MyDirection {
 
   double degrees(double radian) {
     return radian * 180.0 / pi;
+  }
+
+  void guideWithVibration(double destLat, double destLng) async {
+    if (_currentHeading == null || _azimuthDestination == null) return;
+
+    void safeVibration() {
+      Vibration.vibrate(pattern: [100, 400, 100, 400]); // 짧고 반복적인 패턴
+    }
+
+    void warningVibration() {
+      Vibration.vibrate(pattern: [500, 1000, 500, 2000]); // 길고 강한 패턴
+    }
+
+    if (_azimuthDestination! - 20 < 0) {
+      if ((_azimuthDestination! - 20 + 360 <= _currentHeading! &&
+              _currentHeading! <= 360) ||
+          (0 <= _currentHeading! &&
+              _currentHeading! <= _azimuthDestination! + 20)) {
+        safeVibration();
+      } else {
+        warningVibration();
+      }
+    } else if (_azimuthDestination! + 20 >= 360) {
+      if ((_azimuthDestination! - 20 <= _currentHeading! &&
+              _currentHeading! <= 360) ||
+          (0 <= _currentHeading! &&
+              _currentHeading! <= _azimuthDestination! + 20 - 360)) {
+        safeVibration();
+      } else {
+        warningVibration();
+      }
+    } else {
+      if (_azimuthDestination! - 20 <= _currentHeading! &&
+          _currentHeading! <= _azimuthDestination! + 20) {
+        safeVibration();
+      } else {
+        warningVibration();
+      }
+    }
   }
 }
