ccbf63aff2793d75871cc4b5293abf2f47074142
문준형 <moonabcd@naver.com>
2024-05-10 17:34:25 +0900

    feat : regDt 커스텀deserializer 제작

--- data/source/src/main/java/com/sourceconnector/dataset/model/TrafficSignalData.java
+++ data/source/src/main/java/com/sourceconnector/dataset/model/TrafficSignalData.java
@@ -16,15 +16,14 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.sourceconnector.dataset.CustomLocalDateTimeDeserializer;
 
 @JsonIgnoreProperties(ignoreUnknown=true)
 public class TrafficSignalData {
 
 
-	@JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss", timezone = "UTC")
-	@JsonDeserialize(using= LocalDateTimeDeserializer.class)
+	@JsonDeserialize(using = CustomLocalDateTimeDeserializer.class)
 	@JsonSerialize(using= LocalDateTimeSerializer.class)
 	private final LocalDateTime regDt;
 	private final Long itstId;
