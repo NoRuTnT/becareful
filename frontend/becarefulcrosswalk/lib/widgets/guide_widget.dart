540e74ac49a6092438f95550a739ba52a8ef7ddf
김지환 <wlghks98k@gmail.com>
2024-05-14 16:09:30 +0900

    feat: 이용방법 talkback에 맞춰 효과음, 진동 설정

--- frontend/becarefulcrosswalk/lib/widgets/guide_widget.dart
+++ frontend/becarefulcrosswalk/lib/widgets/guide_widget.dart
@@ -5,13 +5,23 @@ import 'package:vibration/vibration.dart';
 
 import '../theme/colors.dart';
 
-class GuideWidget extends StatelessWidget {
-  GuideWidget({
-    super.key,
+class GuideWidget extends StatefulWidget {
+  const GuideWidget({
+    Key? key,
     required this.guide,
     required this.index,
-  });
+    required this.screenIndex,
+  }) : super(key: key);
 
+  final String guide;
+  final int index;
+  final int screenIndex;
+
+  @override
+  _GuideWidgetState createState() => _GuideWidgetState();
+}
+
+class _GuideWidgetState extends State<GuideWidget> {
   final Map<int, Color> colorMap = {
     0: lightRed,
     1: lightYellow,
@@ -25,14 +35,33 @@ class GuideWidget extends StatelessWidget {
     9: strokePurple,
   };
 
-  final String guide;
-  final int index;
+  late FocusNode _focusNode;
 
-  void _onGuidePressed(BuildContext context) async {
-    if (index == 3) {
+  @override
+  void initState() {
+    super.initState();
+    _focusNode = FocusNode();
+    _focusNode.addListener(_handleFocusChange);
+  }
+
+  @override
+  void dispose() {
+    _focusNode.removeListener(_handleFocusChange);
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  void _handleFocusChange() {
+    if (_focusNode.hasFocus) {
+      _onGuidePressed();
+    }
+  }
+
+  void _onGuidePressed() async {
+    if (widget.screenIndex == 1 && widget.index == 3) {
       final player = AudioPlayer();
       await player.play(AssetSource('sounds/half-pass.mp3'));
-    } else if (index == 4) {
+    } else if (widget.screenIndex == 1 && widget.index == 4) {
       Vibration.vibrate(pattern: [100, 400, 100, 400]);
     }
   }
@@ -40,67 +69,72 @@ class GuideWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => _onGuidePressed(context),
-      child: Stack(
-        clipBehavior: Clip.none,
-        children: [
-          Container(
-            margin: const EdgeInsets.only(top: 10, bottom: 30),
-            width: MediaQuery.of(context).size.width * 0.9,
-            decoration: BoxDecoration(
-              color: colorMap[index],
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(
-                color: colorMap[index + 5] ?? Colors.black,
-                width: 1,
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              child: Semantics(
-                sortKey: const OrdinalSortKey(1),
-                label: guide,
-                child: Text(
-                  guide,
-                  style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w200,
-                  ),
-                  textAlign: TextAlign.center,
+      onTap: () => _onGuidePressed(),
+      child: Focus(
+        focusNode: _focusNode,
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 10, bottom: 30),
+              width: MediaQuery.of(context).size.width * 0.9,
+              decoration: BoxDecoration(
+                color: colorMap[widget.index],
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(
+                  color: colorMap[widget.index + 5] ?? Colors.black,
+                  width: 1,
                 ),
               ),
-            ),
-          ),
-          Positioned(
-            top: -15,
-            left: MediaQuery.of(context).size.width / 2 - 40,
-            child: Container(
-              width: 40,
-              height: 40,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(
-                    color: colorMap[index + 5] ?? Colors.black, width: 2.5),
-                color: colorMap[index],
-              ),
-              child: Center(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
                 child: Semantics(
-                  sortKey: const OrdinalSortKey(0),
-                  label: '${index + 1}단계',
+                  sortKey: const OrdinalSortKey(1),
+                  label: widget.guide,
                   child: Text(
-                    '${index + 1}',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.w600,
-                      color: colorMap[index + 5] ?? Colors.black,
+                    widget.guide,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w200,
                     ),
                     textAlign: TextAlign.center,
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+            Positioned(
+              top: -15,
+              left: MediaQuery.of(context).size.width / 2 - 40,
+              child: Container(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                      color: colorMap[widget.index + 5] ?? Colors.black,
+                      width: 2.5),
+                  color: colorMap[widget.index],
+                ),
+                child: Center(
+                  child: Semantics(
+                    sortKey: const OrdinalSortKey(0),
+                    label: '${widget.index + 1}단계',
+                    child: Text(
+                      '${widget.index + 1}',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w600,
+                        color: colorMap[widget.index + 5] ?? Colors.black,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
