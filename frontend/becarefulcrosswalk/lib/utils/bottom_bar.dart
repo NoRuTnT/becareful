71d12b591b20807dae0e0ee1ee57906bafca4160
김지환 <wlghks98k@gmail.com>
2024-05-14 10:24:56 +0900

    reafctor: 페이지 이동 시 스택 pop으로 변경

--- frontend/becarefulcrosswalk/lib/utils/bottom_bar.dart
+++ frontend/becarefulcrosswalk/lib/utils/bottom_bar.dart
@@ -17,13 +17,13 @@ class BottomBar extends StatelessWidget {
         onTap: (int index) {
           switch (index) {
             case 0:
-              Navigator.pushNamed(context, '/userGuide');
+              Navigator.popAndPushNamed(context, '/userGuide');
               break;
             case 1:
-              Navigator.pushNamed(context, '/map');
+              Navigator.popAndPushNamed(context, '/map');
               break;
             case 2:
-              Navigator.pushNamed(context, '/report');
+              Navigator.popAndPushNamed(context, '/report');
               break;
             default:
           }
