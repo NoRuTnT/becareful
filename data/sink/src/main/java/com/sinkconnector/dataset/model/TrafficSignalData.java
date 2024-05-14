ea50539775f313de58360919a08d7d549baf2fd6
문준형 <moonabcd@naver.com>
2024-05-14 17:24:19 +0900

    feat : regDt가 firebase에 저장되는 현상 수정시도

--- data/sink/src/main/java/com/sinkconnector/dataset/model/TrafficSignalData.java
+++ data/sink/src/main/java/com/sinkconnector/dataset/model/TrafficSignalData.java
@@ -16,12 +16,11 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.sinkconnector.dataset.CustomLocalDateTimeDeserializer;
 
-@JsonIgnoreProperties(ignoreUnknown=true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TrafficSignalData {
 
-
 	@JsonDeserialize(using = CustomLocalDateTimeDeserializer.class)
-	@JsonSerialize(using= LocalDateTimeSerializer.class)
+	@JsonSerialize(using = LocalDateTimeSerializer.class)
 	private final LocalDateTime regDt;
 	private final Long itstId;
 
@@ -46,9 +45,8 @@ public class TrafficSignalData {
 	public static String swPdsgRmdrCs_FIELD = "swPdsgRmdrCs";
 	public static String nwPdsgRmdrCs_FIELD = "nwPdsgRmdrCs";
 
-
 	public static Schema schema = SchemaBuilder.struct()
-		.name("com.sourceconnector.dataset.model").version(1).doc("Schema for traffic signal data")
+		.name("com.sinkconnector.dataset.model").version(1).doc("Schema for traffic signal data")
 		.field(regDt_FIELD, Schema.STRING_SCHEMA)
 		.field(itstId_FIELD, Schema.INT64_SCHEMA)
 		.field(ntPdsgRmdrCs_FIELD, Schema.OPTIONAL_FLOAT64_SCHEMA)
@@ -73,7 +71,7 @@ public class TrafficSignalData {
 		@JsonProperty("sePdsgRmdrCs") Double sePdsgRmdrCs,
 		@JsonProperty("swPdsgRmdrCs") Double swPdsgRmdrCs,
 		@JsonProperty("nwPdsgRmdrCs") Double nwPdsgRmdrCs
-		) {
+	) {
 
 		this.regDt = regDt;
 		this.itstId = itstId;
@@ -85,8 +83,6 @@ public class TrafficSignalData {
 		this.sePdsgRmdrCs = sePdsgRmdrCs;
 		this.swPdsgRmdrCs = swPdsgRmdrCs;
 		this.nwPdsgRmdrCs = nwPdsgRmdrCs;
-
-
 
 	}
 
@@ -106,34 +102,34 @@ public class TrafficSignalData {
 			.toEpochSecond(ZoneOffset.UTC);
 	}
 
-
 	public Struct toStruct() {
 		return new Struct(schema)
 			.put(regDt_FIELD, getRegDt().toString())
 			.put(itstId_FIELD, itstId)
-			.put(ntPdsgRmdrCs_FIELD,ntPdsgRmdrCs)
-			.put(etPdsgRmdrCs_FIELD,etPdsgRmdrCs)
-			.put(stPdsgRmdrCs_FIELD,stPdsgRmdrCs)
-			.put(wtPdsgRmdrCs_FIELD,wtPdsgRmdrCs)
-			.put(nePdsgRmdrCs_FIELD,nePdsgRmdrCs)
-			.put(sePdsgRmdrCs_FIELD,sePdsgRmdrCs)
-			.put(swPdsgRmdrCs_FIELD,swPdsgRmdrCs)
-			.put(nwPdsgRmdrCs_FIELD,nwPdsgRmdrCs)
+			.put(ntPdsgRmdrCs_FIELD, ntPdsgRmdrCs)
+			.put(etPdsgRmdrCs_FIELD, etPdsgRmdrCs)
+			.put(stPdsgRmdrCs_FIELD, stPdsgRmdrCs)
+			.put(wtPdsgRmdrCs_FIELD, wtPdsgRmdrCs)
+			.put(nePdsgRmdrCs_FIELD, nePdsgRmdrCs)
+			.put(sePdsgRmdrCs_FIELD, sePdsgRmdrCs)
+			.put(swPdsgRmdrCs_FIELD, swPdsgRmdrCs)
+			.put(nwPdsgRmdrCs_FIELD, nwPdsgRmdrCs)
 			;
 	}
 
 	@Override
 	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		TrafficSignalData that = (TrafficSignalData) o;
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		TrafficSignalData that = (TrafficSignalData)o;
 		return itstId == that.itstId && regDt.equals(that.regDt);
 	}
 
-
-
 	@Override
 	public int hashCode() {
-		return Objects.hash(regDt, itstId, ntPdsgRmdrCs, etPdsgRmdrCs, stPdsgRmdrCs, wtPdsgRmdrCs, nePdsgRmdrCs, sePdsgRmdrCs, swPdsgRmdrCs, nwPdsgRmdrCs);
+		return Objects.hash(regDt, itstId, ntPdsgRmdrCs, etPdsgRmdrCs, stPdsgRmdrCs, wtPdsgRmdrCs, nePdsgRmdrCs,
+			sePdsgRmdrCs, swPdsgRmdrCs, nwPdsgRmdrCs);
 	}
 }
