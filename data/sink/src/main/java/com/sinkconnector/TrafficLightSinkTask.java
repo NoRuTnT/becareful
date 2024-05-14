fe19f895547575edcc21a9068c2ea039d8293ef5
문준형 <moonabcd@naver.com>
2024-05-14 11:00:03 +0900

    feat : put에 디버그용 로그추가

--- data/sink/src/main/java/com/sinkconnector/TrafficLightSinkTask.java
+++ data/sink/src/main/java/com/sinkconnector/TrafficLightSinkTask.java
@@ -11,11 +11,15 @@ import java.util.Map;
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.apache.kafka.connect.sink.SinkTask;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TrafficLightSinkTask extends SinkTask {
+  private static final Logger log = LoggerFactory.getLogger(TrafficLightSinkTask.class);
   private ObjectMapper objectMapper = new ObjectMapper(); // JSON 파서
 
 
@@ -27,12 +31,14 @@ public class TrafficLightSinkTask extends SinkTask {
   }
   @Override
   public void put(Collection<SinkRecord> records) {
+    log.info("put 메소드 실행");
     for (SinkRecord record : records) {
       try {
         String value = (String) record.value(); // 문자열로 읽어오기
         TrafficSignalData trafficSignalData = objectMapper.readValue(value, TrafficSignalData.class); // JSON 파싱
         String id = String.valueOf(trafficSignalData.getItstId());
         if (id != null) {
+          log.info("firebase save data");
           firebaseAdmin.saveData(record.topic(), id, trafficSignalData);
         }
       } catch (Exception e) {
