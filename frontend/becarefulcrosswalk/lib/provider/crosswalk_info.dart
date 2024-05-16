b41358459dc44bb469960a5be261ee33549a9e20
김지환 <wlghks98k@gmail.com>
2024-05-16 10:26:27 +0900

    feat: 현재 신호 정보 받아와서 저장

--- frontend/becarefulcrosswalk/lib/provider/crosswalk_info.dart
+++ frontend/becarefulcrosswalk/lib/provider/crosswalk_info.dart
@@ -3,10 +3,22 @@ import 'package:flutter/cupertino.dart';
 import '../models/intersection_model.dart';
 
 class CrosswalkInfo with ChangeNotifier {
-  Crosswalk? crosswalkInfo; // 0 : 원밖, 1 : 원안, 2 : 직사각형 안 3: 횡단보도 건너기
+  Crosswalk? crosswalkInfo;
+  String? trafficLightState;
+  int? intersectionId;
 
   void setCrosswalkInfo(Crosswalk c) {
     crosswalkInfo = c;
+    notifyListeners();
+  }
+
+  void setTrafficLightState(String s) {
+    trafficLightState = s;
+    notifyListeners();
+  }
+
+  void setIntersectionId(int i) {
+    intersectionId = i;
     notifyListeners();
   }
 }
