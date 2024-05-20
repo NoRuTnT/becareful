fa0ff434b70d23322de78e2f0f5cb118c9c8e76d
문준형 <moonabcd@naver.com>
2024-05-20 13:24:54 +0900

    rebase : 11

--- frontend/becarefulcrosswalk/lib/screens/map_screen.dart
+++ frontend/becarefulcrosswalk/lib/screens/map_screen.dart
@@ -3,7 +3,6 @@ import 'dart:developer';
 
 import 'package:becarefulcrosswalk/utils/bottom_bar.dart';
 import 'package:becarefulcrosswalk/widgets/prompt_widget.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:geofence_service/geofence_service.dart';
@@ -30,8 +29,6 @@ class _MapScreenState extends State<MapScreen> {
   late final MyLocation myLocation;
   late Future<void> initializationFuture;
   late final PolyGeofenceService _polyGeofenceService;
-  late Crosswalk crosswalkInfo;
-  final DatabaseReference _dbRef = FirebaseDatabase.instance.ref();
 
   @override
   void initState() {
@@ -39,24 +36,25 @@ class _MapScreenState extends State<MapScreen> {
     _geofenceService = widget.geofenceService;
     myLocation = MyLocation();
     mapControllerCompleter = Completer<NaverMapController>();
-  }
-
-  Future<void> _initGeofenceService() async {
     _geofenceService.addGeofenceStatusChangeListener(_onGeofenceStatusChanged);
-
-    try {
-      if (Provider.of<MyLocationState>(context, listen: false)
-              .myLocationState ==
-          1) {
-        var geoId =
-            Provider.of<MyLocationState>(context, listen: false).geofenceId;
-        log('나 이미 들어와있어!!!!!!!!!!@@@@@@@@@@@@@@@');
-      }
-      await _geofenceService.start();
-    } catch (e) {
-      print('Error starting geofence service: $e');
-    }
+    _geofenceService.start();
   }
+
+  // Future<void> _initGeofenceService() async {
+  //   _geofenceService.addGeofenceStatusChangeListener(_onGeofenceStatusChanged);
+  //   try {
+  //     if (Provider.of<MyLocationState>(context, listen: false)
+  //             .myLocationState ==
+  //         1) {
+  //       var geoId =
+  //           Provider.of<MyLocationState>(context, listen: false).geofenceId;
+  //       log('나 이미 들어와있어!!!!!!!!!!@@@@@@@@@@@@@@@');
+  //     }
+  //     await _geofenceService.start();
+  //   } catch (e) {
+  //     print('Error starting geofence service: $e');
+  //   }
+  // }
 
   Future<void> _onGeofenceStatusChanged(
       Geofence geofence,
@@ -78,14 +76,16 @@ class _MapScreenState extends State<MapScreen> {
       }
     });
 
-    if (Provider.of<MyLocationState>(context).myLocationState == 1) {
+    if (Provider.of<MyLocationState>(context, listen: false).myLocationState ==
+        1) {
       // 원안에 들어오면
       IntersectionModel intersection =
-          await ApiService.getIntersection(geofence.id as num);
-
+          await ApiService.getIntersection(int.parse(geofence.id));
       await _initPolyGeofenceService(); // PolyGeofence 초기화
+      log("api요청 완료했어");
 
-      for (Crosswalk crosswalk in intersection.crosswalks) {
+      for (Crosswalk crosswalk in intersection.crosswalkList) {
+        log('교차로 아이디: ${crosswalk.crosswalkId}');
         _polyGeofenceService.addPolyGeofence(
           PolyGeofence(
             id: '${crosswalk.crosswalkId}',
@@ -102,6 +102,7 @@ class _MapScreenState extends State<MapScreen> {
             ],
           ),
         );
+        log("끝~!!@");
       }
     }
   }
@@ -129,11 +130,11 @@ class _MapScreenState extends State<MapScreen> {
     setState(() {
       // 횡단보도 직사각형 지오펜스에 들어왔을때
       if (polyGeofenceStatus == PolyGeofenceStatus.ENTER) {
-        Provider.of<MyLocationState>(context, listen: false)
+        polyGeofence.data.crosswalk.Provider
+            .of<MyLocationState>(context, listen: false)
             .setMyLocationState(2); // 위치상태 횡단보도 안
         Provider.of<MyLocationState>(context, listen: false)
             .setGeofenceId(polyGeofence.id);
-        crosswalkInfo = polyGeofence.data.crosswalk;
         if (Provider.of<MyLocationState>(context).myLocationState == 2) {}
       } else if (polyGeofenceStatus == PolyGeofenceStatus.EXIT) {
         Provider.of<MyLocationState>(context, listen: false)
@@ -153,7 +154,7 @@ class _MapScreenState extends State<MapScreen> {
       case 2:
         return '직사각형 안에 보행자 신호가 있습니다.';
       case 3:
-        return "${crosswalkInfo.direction} ${crosswalkInfo.length}미터 횡단보도";
+        return '신호등 건너는 중입니다.';
       default:
         return '반경 20미터 이내에 보행자 신호등이 없습니다.';
     }
@@ -186,106 +187,34 @@ class _MapScreenState extends State<MapScreen> {
         children: [
           PromptWidget(message: getPromptMessage()),
           Expanded(
-            child: FutureBuilder(
-              future: _initGeofenceService(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  return NaverMap(
-                    options: NaverMapViewOptions(
-                      initialCameraPosition: NCameraPosition(
-                          target: NLatLng(
-                              myLocation.latitude, myLocation.longitude),
-                          zoom: 17,
-                          bearing: 0,
-                          tilt: 0),
-                      indoorEnable: true,
-                      // 실내 맵 사용 가능 여부
-                      locationButtonEnable: true,
-                      // 위치 버튼 표시 여부
-                      consumeSymbolTapEvents: true,
-                      // 심볼 탭 이벤트 소비 여부
-                      scrollGesturesEnable: false,
-                      // 스크롤 제스처 비활성화
-                      zoomGesturesEnable: false,
-                      // 확대/축소 제스처 비활성화
-                      rotationGesturesEnable: false,
-                      // 회전 제스처 비활성화
-                      tiltGesturesEnable: false,
-                      // 기울기 제스처 비활성화
-                    ),
-                    onMapReady: (controller) {
-                      mapControllerCompleter.complete(controller);
-                      controller
-                          .setLocationTrackingMode(NLocationTrackingMode.face);
-                      log("onMapReady", name: "onMapReady");
-                    },
-                  );
-                } else {
-                  return const Center(child: CircularProgressIndicator());
-                }
-              },
+              child: NaverMap(
+            options: NaverMapViewOptions(
+              initialCameraPosition: NCameraPosition(
+                  target: NLatLng(myLocation.latitude, myLocation.longitude),
+                  zoom: 17,
+                  bearing: 0,
+                  tilt: 0),
+              indoorEnable: true,
+              // 실내 맵 사용 가능 여부
+              locationButtonEnable: true,
+              // 위치 버튼 표시 여부
+              consumeSymbolTapEvents: true,
+              // 심볼 탭 이벤트 소비 여부
+              scrollGesturesEnable: false,
+              // 스크롤 제스처 비활성화
+              zoomGesturesEnable: false,
+              // 확대/축소 제스처 비활성화
+              rotationGesturesEnable: false,
+              // 회전 제스처 비활성화
+              tiltGesturesEnable: false,
+              // 기울기 제스처 비활성화
             ),
-          ),
-          if (Provider.of<MyLocationState>(context, listen: false)
-                  .myLocationState ==
-              2)
-            SizedBox(
-              height: 55,
-              child: ElevatedButton(
-                onPressed: () {
-                  Provider.of<MyLocationState>(context, listen: false)
-                      .setMyLocationState(3);
-                },
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black,
-                  backgroundColor: const Color(0xFFFFCA41),
-                  side: const BorderSide(
-                    width: 2,
-                    color: Colors.black,
-                  ),
-                  shape: const RoundedRectangleBorder(),
-                  minimumSize: const Size(double.infinity, 20),
-                ),
-                child: const Text(
-                  "안내 시작",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w700,
-                    fontSize: 25,
-                    letterSpacing: 5,
-                  ),
-                ),
-              ),
-            ),
-          if (Provider.of<MyLocationState>(context, listen: false)
-                  .myLocationState ==
-              3)
-            StreamBuilder(
-              stream: _dbRef
-                  .child('traffic-lights/${crosswalkInfo.crosswalkId}')
-                  .onValue,
-              builder: (context, AsyncSnapshot<DatabaseEvent> snapshot) {
-                if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                }
-                if (!snapshot.hasData ||
-                    snapshot.data?.snapshot.value == null) {
-                  return const Center(child: CircularProgressIndicator());
-                }
-                Map<dynamic, dynamic> trafficLightData =
-                    (snapshot.data!.snapshot.value as Map<dynamic, dynamic>);
-
-                String lightColor =
-                    trafficLightData['color'] == "green" ? "초록" : "빨간";
-                Color backgroundColor = trafficLightData['color'] == "green"
-                    ? const Color(0xFF7EC25E)
-                    : const Color(0xFFFD7559);
-
-                return PromptWidget(
-                  message: "$lightColor불 ${trafficLightData['remainingTime']}초",
-                  backgroundColor: backgroundColor,
-                );
-              },
-            ),
+            onMapReady: (controller) {
+              mapControllerCompleter.complete(controller);
+              controller.setLocationTrackingMode(NLocationTrackingMode.face);
+              log("onMapReady", name: "onMapReady");
+            },
+          )),
         ],
       ),
       bottomNavigationBar: const BottomBar(),
