056230dd83f569291fc9f7e0418c4b1c8750277a
문준형 <moonabcd@naver.com>
2024-05-20 13:26:01 +0900

    rebase : 13

---
+++ frontend/becarefulcrosswalk/lib/utils/bottom_bar.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatelessWidget {
+  const BottomBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          top: BorderSide(color: Colors.grey, width: 1.0),
+        ),
+      ),
+      child: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        onTap: (int index) {
+          switch (index) {
+            case 0:
+              Navigator.popAndPushNamed(context, '/userGuide');
+              break;
+            case 1:
+              Navigator.popAndPushNamed(context, '/map');
+              break;
+            case 2:
+              Navigator.popAndPushNamed(context, '/report');
+              break;
+            default:
+          }
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Image.asset('images/information.png', width: 35, height: 35),
+            label: '이용방법',
+          ),
+          BottomNavigationBarItem(
+              icon: Image.asset('images/map.png', width: 35, height: 35),
+              label: '신호정보'),
+          BottomNavigationBarItem(
+              icon: Image.asset('images/report.png', width: 35, height: 35),
+              label: '불편신고'),
+        ],
+      ),
+    );
+  }
+}
